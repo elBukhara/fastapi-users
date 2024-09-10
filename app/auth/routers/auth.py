d3fa7914f92730e3cdf8abef9c6e4896aca95af3
elBukhara <shakhzodgiazov@gmail.com>
2024-09-10 14:33:36 +0300

    added: authorization with bearer token

--- app/auth/routers/auth.py
+++ app/auth/routers/auth.py
@@ -1,12 +1,16 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from fastapi.security import HTTPBearer
 
 from auth.core.backend import authentication_backend
 from auth.core.fastapi_users import fastapi_users
 from auth.schemas.user import UserRead, UserCreate
 
+http_bearer = HTTPBearer(auto_error=False)
+
 router = APIRouter(
     prefix="/auth",
-    tags=["Auth"]
+    tags=["Auth"],
+    dependencies=[Depends(http_bearer)],
 )
 
 # /login - /logout
