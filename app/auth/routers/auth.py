0d42805d953f13526b7ff22c2461245baaf9221b
elBukhara <shakhzodgiazov@gmail.com>
2024-09-11 23:02:53 +0300

    added: optional required verifation on login/

--- app/auth/routers/auth.py
+++ app/auth/routers/auth.py
@@ -15,7 +15,10 @@ router = APIRouter(
 
 # /login - /logout
 router.include_router(
-    router=fastapi_users.get_auth_router(authentication_backend),
+    router=fastapi_users.get_auth_router(
+        authentication_backend,
+        requires_verification=True, # Change it when being on production
+    ),
 )
 
 # /register
